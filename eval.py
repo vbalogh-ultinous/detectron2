b44e779efc58c43f515d05ca764ec6c1f350e40e
vbalogh-ultinous <vbalogh@ultinous.com>
2020-01-09 15:46:50 +0100

    modified eval model

--- eval.py
+++ eval.py
@@ -18,24 +18,32 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
+from detectron2.evaluation import COCOEvaluator
 
 im = cv2.imread("./input.png")
 
 cfg = get_cfg()
 # add project-specific config (e.g., TensorMask) here if you're not running a models in detectron2's core library
 
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml"))
+# cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this models
 
 # Find a models from detectron2's models zoo. You can either use the https://dl.fbaipublicfiles.... url, or use the detectron2:// shorthand
+cfg.MODEL.WEIGHTS = "detectron2://COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x/139686956/model_final_5ad38f.pkl"
+# cfg.MODEL.WEIGHTS = "detectron2://COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x/138363331/model_final_997cc7.pkl"
 # cfg.MODEL.WEIGHTS = "detectron2://COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x/137849621/model_final_a6e10b.pkl"
-cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+# cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
+print(outputs["instances"].pred_keypoints)
+
 
 v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
 v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 cv2.imwrite("./output.png", v.get_image()[:, :, ::-1])
+
+
+# ceval = COCOEvaluator(dataset_name, cfg, True, output_folder)
 
 # cv2.imwrite("./output.png", im)
